f72945a007b7db47ac6122499125206d2cc33028
Harrison Noh <harrisonnoh@Harrisons-MacBook-Pro.local>
2023-03-04 10:39:21 -0800

    added animation

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -20,11 +20,11 @@ export default function Landing() {
                     <h1><span className="text-secondary">Simplify,</span> don&apos;t sacrifice.</h1>
                     <div className="flex self-center md:flex-row flex-col">
                         <p className="my-8">{Strings.Landing}</p>
-                        <animated.div style={{...springs}} className="self-center flex-shrink-0 opacity-0">
+                        <animated.div style={{...springs}} className="self-center flex-shrink-0 opacity-0 m-4">
                             <Image className="self-center" alt="pwr" width={400} height={400} src="/pwr.png" />
                         </animated.div>
                     </div>
-                    <div className="flex">
+                    <div className="flex items-center align-middle">
                         <Button onClick={() => { window.location.href = Strings.Typeform }} className="bg-secondary border-secondary-lighter shadow mr-4 text-white">
                             {Strings.ApplyNow}
                         </Button>
