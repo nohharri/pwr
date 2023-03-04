833d0eb4f7d665feda5a28852a0ff8fe1994c6d8
Harrison Noh <harrisonnoh@Harrisons-MacBook-Pro.local>
2023-03-04 10:24:51 -0800

    added animation to landing

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,8 +1,18 @@
 import Strings from "@/constants/Strings";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
+import { animated, useSpring } from "@react-spring/web";
 
 export default function Landing() {
+
+    const [springs] = useSpring(() => ({ 
+        config: {
+            duration: 1000,
+        },
+        from: { opacity: 0, y: 50 },
+        to: { opacity: 1, y: 0 },
+    }));
+
     return (
         <div className="flex m-auto align-middle items-center max-h-xl sm:h-[calc(100vh-80px)]">
             <div>
@@ -10,7 +20,9 @@ export default function Landing() {
                     <h1><span className="text-secondary">Simplify,</span> don&apos;t sacrifice.</h1>
                     <div className="flex self-center md:flex-row flex-col">
                         <p className="my-8">{Strings.Landing}</p>
-                        <Image className="self-center" alt="pwr" width={400} height={400} src="/pwr.png" />
+                        <animated.div style={{...springs}} className="self-center flex-shrink-0 opacity-0">
+                            <Image className="self-center" alt="pwr" width={400} height={400} src="/pwr.png" />
+                        </animated.div>
                     </div>
                     <div className="flex">
                         <Button onClick={() => { window.location.href = Strings.Typeform }} className="bg-secondary border-secondary-lighter shadow mr-4 text-white">
